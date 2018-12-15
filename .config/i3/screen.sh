4c5894295313a1ada6729d77fa61e216d37bffb7
Davis Kregers <kregers.davis@gmail.com>
2018-12-15 14:45:57 +0200

    Initial commit

---
+++ .config/i3/screen.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-2 --off --output HDMI-1 --mode 1920x1200 --pos 1912x0 --rotate normal --output DP-1 --off --output DVI-D-1 --mode 1920x1200 --pos 0x0 --rotate normal
---
+++ sync.sh
@@ -0,0 +1,6 @@
+# /bin/bash
+
+ln -sv ~/dotfiles/.bash_profile ~
+ln -sv ~/dotfiles/i3 ~/.config
+ln -sv ~/dotfiles/.Xauthority ~
+ln -sv ~/dotfiles/.xinitrc ~
