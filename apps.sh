c6f9f9af4f11935c2ffa975d12795dd2a6ea6fc0
Davis <davis.kregers@edurio.com>
2018-12-16 13:06:02 +0200

    Reinstall vscode using snap

--- apps.sh
+++ apps.sh
@@ -1,5 +1,5 @@
 pacman -S redshift thunar zip unzip lxappearance termite xdg-utils playerctl openssh
-yaourt -S spotify playerctl code betterlockscreen feh snapd
+yaourt -S spotify playerctl betterlockscreen feh snapd
 systemctl enable --now apparmor.service
 systemctl enable --now snapd.apparmor.service
 systemctl restart snapd
@@ -8,4 +8,8 @@ sudo snap install gitkraken
 sudo chmod +x /etc/profile.d/snapd.sh 
 cd /etc/profile.d/ && ./snapd.sh
 
+sudo ln -s /var/lib/snapd/snap /snap
+sudo snap install vscode --classic
+
 sudo ln -s /var/lib/snapd/snap/bin/gitkraken /usr/bin/
+sudo ln -s /var/lib/snapd/snap/bin/code /var/lib/snapd/snap/code
