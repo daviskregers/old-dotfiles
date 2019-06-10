8e0fa03601653b726a13a154d9a6e14beb3e74a8
Dāvis Krēgers <daviskregers@protonmail.com>
2019-06-10 22:10:22 +0300

    some apps

--- apps.sh
+++ apps.sh
@@ -4,7 +4,8 @@ sudo pacman -Syu
 sudo pacman -S --noconfirm --needed ttf-dejavu redshift thunar zip unzip \
 lxappearance termite xdg-utils playerctl openssh gedit xarchiver ntp conky \
 conky-manager zsh pambase xclip wine docker docker-compose muparser numlockx \
-filezilla the_silver_searcher ranger xdotool pulseaudio lib32-libpulse lib32-alsa-plugins pulseaudio-alsa pavucontrol ctags xcompmgr
+filezilla the_silver_searcher ranger xdotool pulseaudio lib32-libpulse lib32-alsa-plugins \
+pulseaudio-alsa pavucontrol ctags xcompmgr mpv ffmpeg wireshark-qt
 
 sudo systemctl enable ntpd.service
 sudo systemctl start ntpd.service
@@ -21,9 +22,13 @@ gpg --recv-keys A2C794A986419D8A
 yaourt -Syu
 yaourt -S --noconfirm --needed google-chrome ttf-ms-fonts spotify playerctl \
 betterlockscreen feh discord slack-desktop postman-bin shutter perl-goo-canvas \
-heidisql xkb-switch powerline-fonts albert nixnote2-git yad paswitch speedcrunch vim-youcompleteme-git compton-conf
+heidisql xkb-switch powerline-fonts albert nixnote2-git yad paswitch speedcrunch \
+vim-youcompleteme-git compton-conf shantz-xwinwrap-bzr pgadmin3
 
 yaourt -S --needed vi-vim-symlink
 
 systemctl --user enable pulseaudio
 chmod +x ~/.dotfiles/scripts/*
+
+sudo usermod -aG wireshark $USER
+groups
