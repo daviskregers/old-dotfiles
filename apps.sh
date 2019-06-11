31012fadc40fc71aac93b0f8311ae0666ddab7ff
Dāvis Krēgers <daviskregers@protonmail.com>
2019-06-11 21:47:15 +0300

    add java jdk

--- apps.sh
+++ apps.sh
@@ -5,7 +5,7 @@ sudo pacman -S --noconfirm --needed ttf-dejavu redshift thunar zip unzip \
 lxappearance termite xdg-utils playerctl openssh gedit xarchiver ntp conky \
 conky-manager zsh pambase xclip wine docker docker-compose muparser numlockx \
 filezilla the_silver_searcher ranger xdotool pulseaudio lib32-libpulse lib32-alsa-plugins \
-pulseaudio-alsa pavucontrol ctags xcompmgr libreoffice aws-cli wireshark-qt
+pulseaudio-alsa pavucontrol ctags xcompmgr libreoffice aws-cli wireshark-qt jdk-openjdk
 
 sudo systemctl enable ntpd.service
 sudo systemctl start ntpd.service
