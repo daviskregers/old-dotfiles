bcc0ad0809c91db5657f7f56a57d48b5662327d6
Dāvis Krēgers <davis.kregers@edurio.com>
2019-11-05 18:05:59 +0200

    change redshift brightness

--- monitor-layout.sh
+++ monitor-layout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-2 --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI-1 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP-1 --off --output DVI-D-1 --off
+xrandr --output DP-1 --off --output HDMI-1 --mode 1920x1200 --pos 1920x0 --rotate normal --output DVI-D-1 --off --output DVI-D-2 --mode 1920x1200 --pos 0x0 --rotate normal
