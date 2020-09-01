cf9b264e49a442225fe3fb9f99a990d1a8565cad
Davis Kregers <davis.kregers@edurio.com>
2020-09-01 10:45:24 +0300

    add colors

--- install-arch.sh
+++ install-arch.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+source .colors
+
 SEPARATOR="------------------------------------------"
 
 if [ $(uname -n ) != "archiso" ]; then
-	echo "This isn't an arch installation, exiting...";
+	echo -e "${BLINK}This isn't an arch installation, exiting...$END"
 	exit 1;
 fi
 
-echo "Arch installation"
-echo $SEPARATOR
+echo -e "${GREEN}Arch installation $END"
+echo -e $SEPARATOR
 
-echo "
-Devices available: 
+echo -e "
+Devices available:
 "
 lsblk
-echo $SEPARATOR
-echo "
-The next step is to format drives. Make sure that you specify the correct one from the device list.
+echo -e $SEPARATOR
+echo -e "$RED
+The next step is to format drives. Make sure that you specify the correct one from the device list. $END
 Format: /dev/sdX
 "
